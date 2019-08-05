44b19d7dca70c8720b5877a6551a2194f58644e7
Harry Saito <hhsaito@Harrys-iMac.local>
2019-08-04 22:04:55 -0700

    First files

---
+++ js/validator.js
@@ -0,0 +1,37 @@
+$(function() {
+    // Initialize form validation on the registration form.
+    // It has the name attribute "registration"
+    $("form[name='testform']").validate({
+        // Specify validation rules
+        rules: {
+            // The key name on the left side is the name attribute
+            // of an input field. Validation rules are defined
+            // on the right side
+            name: "required",
+            email: {
+                required: true,
+                // Specify that email should be validated
+                // by the built-in "email" rule
+                email: true
+            },
+            phone: {
+                required: true,
+            },
+            msg: {
+                required: true
+            },
+        },
+        // Specify validation error messages
+        messages: {
+            name: "Please enter your name",
+            email: "Please enter a valid email address",
+            phone: "Please enter a valid U.S. phone number",
+            msg: "Please enter your message",
+        },
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
+        submitHandler: function(form) {
+        form.submit();
+        }
+    });
+});
